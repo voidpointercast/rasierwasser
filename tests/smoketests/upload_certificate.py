bfedc80ca66927b1992612efb79db56e5b1291fd
voidpointercast <patrick.daniel.gress@gmail.com>
2021-06-04 02:21:25 +0200

    Added auth

--- tests/smoketests/upload_certificate.py
+++ tests/smoketests/upload_certificate.py
@@ -3,12 +3,14 @@
 from argparse import ArgumentParser
 from base64 import b64encode
 from requests import post
-
+from requests.auth import HTTPBasicAuth
 
 def main():
     parser: ArgumentParser = ArgumentParser(description='Upload a certificate file')
     parser.add_argument('name', help='Name of the certificate.')
     parser.add_argument('file', help='Path to the certificate to upload.')
+    parser.add_argument('--username', default='alice', help='Username for authentication.')
+    parser.add_argument('--password', default='bob', help='Password for authentication.')
     parser.add_argument('--port', type=int, default=10010, help='Rasierwasser port.')
     parser.add_argument('--host', default='localhost', help='Rasierwasser host.')
 
@@ -16,7 +18,8 @@ def main():
     with open(args.file, 'rb') as src:
         with post(
                 f'http://{args.host}:{args.port}/certificates',
-                json=dict(name=args.name, public_key_base64=b64encode(src.read()).decode())
+                json=dict(name=args.name, public_key_base64=b64encode(src.read()).decode()),
+                auth=HTTPBasicAuth(username=args.username, password=args.password)
         ) as response:
             print(response.status_code, response.text)
 
