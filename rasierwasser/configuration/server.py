f535ad38fd875112d9812bab82457d102f17a88d
voidpointercast <patrick.daniel.gress@gmail.com>
2021-06-04 02:22:21 +0200

    Added auth config support

--- rasierwasser/configuration/server.py
+++ rasierwasser/configuration/server.py
@@ -1,7 +1,23 @@
-from pydantic import BaseModel
+from typing import Dict, Any, TypeVar
+from pydantic import BaseModel, Field
+
+
+class AuthConfig(BaseModel):
+    policy: str
+    parameter: Dict[str, Any] = Field(default_factory=dict)
 
 
 class ServerConfig(BaseModel):
     hostname: str
     port: int
     debug: bool = False
+
+
+SecurityScheme = TypeVar('SecurityScheme')
+
+DEFAULT_AUTH_CONFIG = AuthConfig(policy='snakeoil')
+
+
+
+
+
