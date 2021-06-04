d3b64fe6fdec42663e82a2cac157b12a3db28fc4
voidpointercast <patrick.daniel.gress@gmail.com>
2021-06-04 02:24:42 +0200

    Respecting auth in default_server

--- rasierwasser/server/__init__.py
+++ rasierwasser/server/__init__.py
@@ -1,9 +1,10 @@
 from typing import TypeVar
 from rasierwasser.server.fastapi import create_fastapi_server
 from rasierwasser.storage.algebra import Storage
+from rasierwasser.configuration.main import AuthConfig, DEFAULT_AUTH_CONFIG
 
 WSGIServer = TypeVar('WSGIServer')
 
 
-def default_server(storage: Storage, debug: bool = False) -> WSGIServer:
-    return create_fastapi_server(storage, debug)
+def default_server(storage: Storage, debug: bool = False, auth: AuthConfig = DEFAULT_AUTH_CONFIG) -> WSGIServer:
+    return create_fastapi_server(storage, debug, auth)
